4f14c4bbae90bfa66031c0c4dbe23882b793f2ee
Ajay Khandla <ajay.khandla97@gmail.com>
2025-12-18 11:32:32 +0530

    fix setu pnpm

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,35 @@
-module.exports = [];
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+const reactPlugin = require('eslint-plugin-react');
+const reactHooks = require('eslint-plugin-react-hooks');
+
+module.exports = [
+  {
+    ignores: ['dist', 'node_modules', 'coverage', '**/dist/**', 'tmp']
+  },
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      react: reactPlugin,
+      'react-hooks': reactHooks
+    },
+    settings: {
+      react: { version: 'detect' }
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...reactPlugin.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-empty-interface': 'off'
+    }
+  }
+];
